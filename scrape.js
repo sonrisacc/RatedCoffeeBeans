5a17555866b365aa063df86fecbb954e0166cfbd
Sonrisa <sonrisa722611@gmail.com>
2017-10-31 19:42:16 -0700

    Update basic multi page scraping function with promise

--- scrape.js
+++ scrape.js
@@ -5,13 +5,13 @@ var page1 = 'http://www.coffeereview.com/highest-rated-coffees/page/1';
 var page2 = 'http://www.coffeereview.com/highest-rated-coffees/page/2';
 
 var links = [page, page1, page2];
-
+var result = [];
 function scrapeUrl(url) {
-  let scrapeOneUrl = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     request(url, function(error, response, html) {
       if (!error && response.statusCode == 200) {
-        var $ = cheerio.load(html);
         var output = [];
+        var $ = cheerio.load(html);
         $('div[class=review-content]').each(function(i, element) {
           var rating = $(this)
             .children()
@@ -54,25 +54,32 @@ function scrapeUrl(url) {
             .attr('href');
 
           var metadata = {
-            rating: parseInt(rating),
-            brand: brand,
-            bean: bean,
-            reviewDate: reviewDate,
-            price: price,
-            brandUrl: brandUrl,
-            beanUrl: beanUrl
+            rating: parseInt(rating)
+            // brand: brand,
+            // bean: bean,
+            // reviewDate: reviewDate,
+            // price: price,
+            // brandUrl: brandUrl,
+            // beanUrl: beanUrl
           };
           output.push(metadata);
         });
       }
       resolve(output);
     });
-  });
-  scrapeOneUrl
+  })
     .then(data => {
-      console.log(data.length);
-      return data.length;
+      return data;
     })
     .catch(err => console.log(err));
 }
-links.reduce((acc, cur) => acc + scrapeUrl(cur));
+
+// Promise.all([scrapeUrl(page1), scrapeUrl(page)]).then(value => {
+//   console.log(value);
+//   return value;
+// });
+
+Promise.all(links.map(cur => scrapeUrl(cur))).then(value => {
+  console.log(value);
+  return value;
+});
